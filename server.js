3b670ca792d49440342af6b5d2df70d46af169d0
MatexConcept <matexconcept@gmail.com>
2023-09-08 16:39:36 +0100

    second-commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://authz-app.vercel.app"],
+    origin: ["http://localhost:5173", "https://opa-pay.vercel.app"],
     credentials: true,
   })
 );
