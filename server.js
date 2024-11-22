be96194b3eee14785fc676ec787f418660dbd7c8
MatexConcept <matexconcept@gmail.com>
2024-11-22 15:40:02 +0100

    update

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://opa-pay.vercel.app"],
+    origin: ["http://localhost:5173", "https://opapay-frontend.vercel.app/"],
     credentials: true,
   })
 );
